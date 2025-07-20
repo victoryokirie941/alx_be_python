ae8cb10ba30cf3487a2852e56555d20c9be83a95
victoryokirie <victoryokirie98@gmail.com>
2025-07-20 17:50:16 +0100

    renamed file multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+# Prompt the user to enter a number
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and print the multiplication table from 1 to 10
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
